58cf3690d4086ad138b534afd9b266e6adf05d2f
Jon Lewis <lewis.jonathan.n@gmail.com>
2020-03-28 21:42:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,32 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var itemName
+ var itemPrice
+ var randomNumber = Math.floor(Math.random() * 100) + 1
+ for (let i = 0; i < item.length; i++) {
+  var itemsAddedToCart = Object.assign({itemName: item}, {itemPrice: randomNumber})
+ }
+ cart.push(itemsAddedToCart)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  
 }
 
 function total() {
   // write your code here
+  var total = 0
+  for (let i = 0; i < cart.length; i++) {
+    var items = cart[i]
+    var itemPriceObject = Object.values(items)
+    var itemPriceArray = []
+    itemPriceArray.push(itemPriceArray)
+    total += itemPriceArray[i]
+  }
+  return total
 }
 
 function removeFromCart(item) {
